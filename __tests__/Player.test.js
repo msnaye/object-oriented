57f6b09959ca950123b2aca3def683198b23aca4
msnaye <99111936+msnaye@users.noreply.github.com>
2022-05-22 01:34:21 -0500

    done with player object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -52,6 +52,7 @@ jest.mock('../lib/Potion.js');
   
     expect(player.isAlive()).toBeFalsy();
   });
+
   test("subtracts from player's health", () => {
     const player = new Player('Dave');
     const oldHealth = player.health;
@@ -64,6 +65,32 @@ jest.mock('../lib/Potion.js');
   
     expect(player.health).toBe(0);
   });
+
+  test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
   
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });  
+
+  test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+  
+    player.addPotion(new Potion());
+  
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
+  });
   
   
